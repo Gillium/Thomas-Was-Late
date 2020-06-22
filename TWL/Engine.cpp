67445d93cb137d3d6e74bc7dbbdc4d5103a180e3
Peter Piper <petermpiper@gmail.com>
2020-06-22 06:48:22 -0400

    Coded the Engine class constructor definition
    Coded the Engine class constructor definition

---
+++ TWL/Engine.cpp
@@ -0,0 +1,30 @@
+#include "Engine.h"
+
+Engine::Engine()
+{
+	// Get the screen resolution
+	// and create an SFML window and View
+	Vector2f resolution;
+	resolution.x = VideoMode::getDesktopMode().width;
+	resolution.y = VideoMode::getDesktopMode().height;
+
+	m_Window.create(VideoMode(resolution.x, resolution.y), "Thomas was late", Style::Fullscreen);
+
+	// Initialize the full screen view
+	m_MainView.setSize(resolution);
+	m_HudView.reset(FloatRect(0, 0, resolution.x, resolution.y));
+
+	// Initialize the split-screen Views
+	m_LeftView.setViewport(FloatRect(0.001f, 0.001f, 0.498f, 0.998f));
+
+	m_RightView.setViewport(FloatRect(0.5f, 0.001f, 0.499f, 0.998f));
+
+	m_BGLeftView.setViewport(FloatRect(0.001f, 0.001f, 0.498f, 0.998f));
+
+	m_BGRightView.setViewport(FloatRect(0.5f, 0.001f, 0.499f, 0.998f));
+
+	m_BackgroundTexture = TextureHolder::GetTexture("graphics/background.png");
+
+	// Associate the sprite with the texture
+	m_BackgroundSprite.setTexture(m_BackgroundTexture);
+}
