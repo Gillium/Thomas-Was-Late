ea8e36897b92387765fb6399d962e6a6907c825f
Peter Piper <petermpiper@gmail.com>
2020-06-23 09:38:35 -0400

    Added Bob and Thomas to the draw function
    Added Bob and Thomas to the draw function

--- TWL/Draw.cpp
+++ TWL/Draw.cpp
@@ -13,6 +13,12 @@ void Engine::draw()
 		m_Window.draw(m_BackgroundSprite);
 		// Switch to m_MainView
 		m_Window.setView(m_MainView);
+
+		// Draw Thomas
+		m_Window.draw(m_Thomas.getSprite());
+
+		// Draw Bob
+		m_Window.draw(m_Bob.getSprite());
 	}
 	else
 	{
@@ -27,6 +33,12 @@ void Engine::draw()
 		// Switch to m_LeftView
 		m_Window.setView(m_LeftView);
 
+		// Draw Bob
+		m_Window.draw(m_Bob.getSprite());
+
+		// Draw Thomas
+		m_Window.draw(m_Thomas.getSprite());
+
 		// Now draw Bob's side of the screen
 
 		// Switch to background view
@@ -35,6 +47,12 @@ void Engine::draw()
 		m_Window.draw(m_BackgroundSprite);
 		// Switch to m_RightView
 		m_Window.setView(m_RightView);
+
+		// Draw Thomas
+		m_Window.draw(m_Thomas.getSprite());
+		
+		// Draw Bob
+		m_Window.draw(m_Bob.getSprite());
 	}
 
 	// Draw the HUD
